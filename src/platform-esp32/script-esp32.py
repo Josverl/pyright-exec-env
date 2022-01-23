781583643c4c23f6d92bac38f80a281f06078b4b
Jos Verlinde <jos.verlinde@microsoft.com>
2022-01-23 16:54:57 +0100

    rename folders. add Logs

--- src/platform-esp32/script-esp32.py
+++ src/platform-esp32/script-esp32.py
@@ -5,5 +5,3 @@ import esp32 # should import from esp32     - OK
 import esp  # should import from esp32      - OK 
 import pyb # Should Fail to import          - INCORRECT  - imports from 3rd exec environment
 
-
-
